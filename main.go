3b9920e62148c162372dbabe9eecd177169e1b5b
celso.rodrigues <celso.rodrigues@gocity.com>
2024-04-17 17:04:18 +0100

    v9

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/http/httputil"
 	"os"
 )
 
@@ -10,18 +13,48 @@ var (
 	version string
 )
 
-func main() {
-	fmt.Println(version)
-	writeIgnore()
+type data struct {
+	project string
+	vuln    string
 }
 
-func writeIgnore() {
+func main() {
+	fmt.Println(version)
+	cve, err := getCVEs()
+	if err != nil {
+		log.Println(err)
+	}
+	writeIgnore(cve)
+}
+
+func writeIgnore(cve []byte) {
 	f, err := os.OpenFile(".trivyignore", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	_, err = f.Write([]byte("CVE-2022-48174"))
+	_, err = f.Write(cve)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
+}
+
+func getCVEs(serverAddress, serverPort, projectName string) ([]byte, error) {
+	data := data{}
+	client := http.Client{}
+	url := fmt.Sprintf("%s:%s/%s", serverAddress, serverPort, projectName)
+
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+
+	httputil.DumpResponse(resp, true)
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return []byte("CVE-2022-48174"), nil
+
 }
