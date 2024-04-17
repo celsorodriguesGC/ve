a587b74710307c9a16636309e956f0ea8e7331f9
celso.rodrigues <celso.rodrigues@gocity.com>
2024-04-17 15:46:57 +0100

    v8

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 var (
 	version string
@@ -8,4 +12,16 @@ var (
 
 func main() {
 	fmt.Println(version)
+	writeIgnore()
+}
+
+func writeIgnore() {
+	f, err := os.OpenFile(".trivyignore", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = f.Write([]byte("CVE-2022-48174"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
